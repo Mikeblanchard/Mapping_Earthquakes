aeabc57a190e32e143d9d6fddeb36148633d2617
Mikeblanchard <76125201+Mikeblanchard@users.noreply.github.com>
2021-03-26 23:37:52 -0400

    challenge files.

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -16,7 +16,7 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 });
 
 // We create the second tile layer that will be the background of our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
@@ -32,8 +32,8 @@ let map = L.map('mapid', {
 
 // Create a base layer that holds all three maps.
 let baseMaps = {
-  "Satellite": satelliteStreets,
   "Streets": streets,
+  "Satellite": satelliteStreets,
   "Dark": dark
 };
 
@@ -212,10 +212,12 @@ legend.onAdd = function() {
 // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
 d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function(data) {
   L.geoJson(data, {
-    color: 'yellow',
-    weight: 1
+    color: '#fefe4a',
+    dashArray: '5, 5',
+    opacity: ".75",
+    weight: 3
   })
   .addTo(tPlates);
   tPlates.addTo(map);
-});
+  });
 });
